c8deefe42823d8a8cb45cd20567660e5a2714bf2
sahilsapariya <sahilsapariya03@gmail.com>
2023-12-06 21:29:40 +0530

    initial setup

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
+const Home = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Home
