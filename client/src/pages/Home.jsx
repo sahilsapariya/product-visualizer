0f06062e2bd636e45fd9d47d4f72c474eab793d3
sahilsapariya <sahilsapariya03@gmail.com>
2023-12-06 22:54:43 +0530

    created home page and customizer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import {
+  headContentAnimation,
+  headContainerAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion.js";
+import state from "../store/index.js";
+import { CustomButton } from "../components";
+
+
+
 
 const Home = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const snap = useSnapshot(state);
 
-export default Home
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+          <motion.header {...slideAnimation('down')}>
+            <img src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" />
+              </h1>
+            </motion.div>
+            <motion.div {...headContentAnimation} 
+            className="flex flex-col gap-5">
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tool. <strong>Unlease your imagination</strong>{" "}
+                and define your own style.
+              </p>
+              
+              <CustomButton
+                type="filled"
+                title="Customize it"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+
+            </motion.div>
+
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
